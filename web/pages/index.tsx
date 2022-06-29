7f298c81713e8410b19cfed600ba6faa7a9e85a5
muraikenta <muraikenta0507@gmail.com>
2022-06-29 17:27:53 +0900

    add example of getSDK

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,11 @@
-import { ApolloClient, gql, InMemoryCache, useQuery } from '@apollo/client'
+// import { ApolloClient, gql, InMemoryCache, useQuery } from '@apollo/client'
+import { GraphQLClient } from 'graphql-request'
+
 import { Box } from '@mui/material'
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
+import { getSdk } from '../src/generated/graphql'
+import { gql } from '@apollo/client'
 
 const EXCHANGE_RATES = gql`
   query GetExchangeRates {
@@ -14,21 +18,24 @@ const EXCHANGE_RATES = gql`
 `
 
 interface ExchangeRate {
-  name: string
-  currency: string
-  rate: string
+  name?: string | null
+  currency?: string | null
+  rate?: string | null
 }
 
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache(),
-})
+// const client = new ApolloClient({
+//   uri: 'https://48p1r2roz4.sse.codesandbox.io',
+//   cache: new InMemoryCache(),
+// })
+
+const sdk = getSdk(new GraphQLClient('https://48p1r2roz4.sse.codesandbox.io'))
 
 async function fetchData() {
-  const result = await client.query<{ rates: ExchangeRate[] }>({
-    query: EXCHANGE_RATES,
-  })
-  return result.data
+  // const result = await client.query<{ rates: ExchangeRate[] }>({
+  //   query: EXCHANGE_RATES,
+  // })
+  // return result.data
+  return (await sdk.GetExchangeRates()).rates as ExchangeRate[]
 }
 
 const Home: NextPage = () => {
@@ -37,7 +44,7 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     fetchData().then(data => {
-      setData(data.rates)
+      setData(data)
       setLoading(false)
     })
   }, [])
